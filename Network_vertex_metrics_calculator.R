a6c6f315424c2604cec015de091f68c70ef809b6
Manish Jain <manishjain2792@users.noreply.github.com>
2015-05-08 23:24:20 +0530

    Update Network_vertex_metrics_calculator.R
    added some changes for directed networks.

--- Network_vertex_metrics_calculator.R
+++ Network_vertex_metrics_calculator.R
@@ -8,61 +8,63 @@ filenames <- list.files(path=directory,pattern="*.net")
 man<-typeof(filenames)
 len=length(filenames)
 
-prompt <- "Select the metrics you want to calculate (space-separated list) \n
-1.Degree\n2.Betweenness\n3.Clustering Coeff\n4.Closeness\n5.Eigen Centrality\n6.PageRank \n"
+prompt <- "Select the metrics you want to calculate (space-separated list) (Don't choose 1 and 2 for un-directed)\n
+1.Indegree\n2.Outdegree\n3.Degree\n4.Betweenness\n5.Clustering Coeff\n6.Closeness\n7.Eigen Centrality\n8.PageRank \n"
 EXP <- as.integer(strsplit(readline(prompt), " ")[[1]])
 print(choice)
 if (choice==1){
   
-
-for(i in 1:len)
-{
   
-  ptr <- filenames[i]  
-  temp<- file_path_sans_ext(ptr)
-  #print(ptr)
-  if(file.exists(ptr))
+  for(i in 1:len)
   {
-    setwd(file.path(paste(directory)))
-  }
-  else
-  {
-    dir.create(file.path(paste(directory)))
-    setwd(file.path(paste(directory)))
-  }
-  name <- paste(directory,ptr,sep="")
-  x <- read.graph(name,format="pajek")
-  
-  degree <- degree(x,v=V(x),mode=c("all"))
-  
-  betweenness<-betweenness(x,v=V(x), directed=TRUE)
-  
-  closeness <- closeness(x, vids=V(x),mode=c("all"),normalized=TRUE)
-  
-  pagerank_temp<-page.rank(x,vids=V(x),directed=TRUE)
-  
-  pagerank<-pagerank_temp$vector
-  
-  clustcoeff <-transitivity(x,type=c("local"))
-  
-  eigencentrality_temp <-evcent(x,directed=TRUE)
-  
-  eigencentrality<-eigencentrality_temp$vector    
-  
-  file1=paste(temp,"-metrics",".csv",sep="")
-  
- 
-  write.table(cbind(degree,betweenness,clustcoeff,closeness,eigencentrality,pagerank), 
-              file1, row.names = FALSE,
-              col.names=c('degree','betweenness','clustcoeff','closeness','eigencentrality','pagerank'),sep=",",na="0")
-  
-  
-  file <- read.csv(file1)
-  
-  small <- subset(file,,EXP)
-  
-  write.csv(small,file1)
-  
+    
+    ptr <- filenames[i]  
+    temp<- file_path_sans_ext(ptr)
+    #print(ptr)
+    if(file.exists(ptr))
+    {
+      setwd(file.path(paste(directory)))
+    }
+    else
+    {
+      dir.create(file.path(paste(directory)))
+      setwd(file.path(paste(directory)))
+    }
+    name <- paste(directory,ptr,sep="")
+    x <- read.graph(name,format="pajek")
+    
+    degree_in <- degree(x,v=V(x),mode=c("in"))
+    degree_out<-degree(x,v=V(x),mode=c("out"))
+    degree<-degree(x,v=V(x),mode=c("total"))
+    
+    betweenness<-betweenness(x,v=V(x), directed=TRUE)
+    
+    closeness <- closeness(x, vids=V(x),mode=c("all"),normalized=TRUE)
+    
+    pagerank_temp<-page.rank(x,vids=V(x),directed=TRUE)
+    
+    pagerank<-pagerank_temp$vector
+    
+    clustcoeff <-transitivity(x,type=c("local"))
+    
+    eigencentrality_temp <-evcent(x,directed=TRUE)
+    
+    eigencentrality<-eigencentrality_temp$vector    
+    
+    file1=paste(temp,"-metrics",".csv",sep="")
+    
+    
+    write.table(cbind(degree_in,degree_out,degree,betweenness,clustcoeff,closeness,eigencentrality,pagerank), 
+                file1, row.names = FALSE,
+                col.names=c('in_degree','out_degree','degree','betweenness','clustcoeff','closeness','eigencentrality','pagerank'),sep=",",na="0")
+    
+    
+    file <- read.csv(file1)
+    
+    small <- subset(file,,EXP)
+    
+    write.csv(small,file1)
+    
   }
 }
 if (choice==2)
@@ -84,6 +86,8 @@ if (choice==2)
     name <- paste(directory,ptr,sep="")
     x <- read.graph(name,format="pajek")
     
+    degree_in <-degree(x,v=V(x))
+    degree_out<-degree(x,v=V(x))
     degree <- degree(x,v=V(x))
     
     betweenness<-betweenness(x,v=V(x), directed=FALSE)
@@ -102,15 +106,15 @@ if (choice==2)
     
     file1=paste(temp,"-metrics",".csv",sep="")
     
-    write.table(cbind(degree,betweenness,clustcoeff,closeness,eigencentrality,pagerank), 
+    write.table(cbind(degree_in,degree_out,degree,betweenness,clustcoeff,closeness,eigencentrality,pagerank), 
                 file1, row.names = FALSE,
-                col.names=c('degree','betweenness','clustcoeff','closeness','eigencentrality','pagerank'),sep=",", na="0")
+                col.names=c('in_degree','out_degree','degree','betweenness','clustcoeff','closeness','eigencentrality','pagerank'),sep=",", na="0")
     
     
     file <- read.csv(file1)
     
     small <- subset(file,,EXP)
-    
+    print (EXP)
     write.csv(small,file1)
     
   }
